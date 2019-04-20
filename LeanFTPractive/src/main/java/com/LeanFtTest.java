bdb7269ae21e72b9e2d4aaae8b9136763094452a
Mohammed.Nazeer <mohammed.nazeer@emaratech.ae>
2019-04-20 15:23:37 +0400

    Initial commit

---
+++ LeanFTPractive/src/main/java/com/LeanFtTest.java
@@ -0,0 +1,35 @@
+package com;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import com.hp.lft.sdk.*;
+import com.hp.lft.verifications.*;
+
+import unittesting.*;
+
+public class LeanFtTest extends UnitTestClassBase {
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public void afterClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void beforeMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void afterMethod() throws Exception {
+    }
+
+    @Test
+    public void test() throws GeneralLeanFtException {
+    }
+
+}
---
+++ LeanFTPractive/src/test/java/PackageLogin/Login.java
@@ -0,0 +1,35 @@
+package PackageLogin;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import com.hp.lft.sdk.*;
+import com.hp.lft.verifications.*;
+
+import unittesting.*;
+
+public class Login extends UnitTestClassBase {
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public void afterClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void beforeMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void afterMethod() throws Exception {
+    }
+
+    @Test
+    public void test() throws GeneralLeanFtException {
+    }
+
+}
