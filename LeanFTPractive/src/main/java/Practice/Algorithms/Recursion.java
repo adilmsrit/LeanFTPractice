1ae53993b59f146dbf75b0baccc73349ca0d5f09
Mohammed Adil Nazeer <mohammed.nazeer@emaratech.ae>
2019-08-05 12:16:56 +0400

    v

--- LeanFTPractive/src/main/java/Practice/Algorithms/Recursion.java
+++ LeanFTPractive/src/main/java/Practice/Algorithms/Recursion.java
@@ -1,5 +1,8 @@
 package Practice.Algorithms;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Recursion {
 
     public static void rec(int x) {
@@ -34,10 +37,48 @@ public class Recursion {
         return result;
     }
 
+    public static int maxTriple(int[] nums) {
+        int first = nums[0];
+        int last = nums[nums.length - 1];
+        int middle = nums[(nums.length / 2)];
+
+        System.out.println(" " + first + " " + middle + " " + last);
+
+        if (first > middle && first > last) return first;
+
+        if (middle > first && middle > last) return middle;
+
+        if (last > middle && last > first) return last;
+        return 0;
+    }
+
+    public static void bubbleSort(int nums[]) {
+        for (int i = nums.length-1; i >=0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp;
+                    temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
     public static void main(String[] args) {
 
-        rec(4);
+      /*  rec(4);
         System.out.println(power(2, 10));
         System.out.println(factorial(8));
+        */
+        int[] adil = {32,1, 5, 3};
+
+
+//        System.out.println(maxTriple(adil));
+
+        System.out.println(Arrays.toString(adil));
+        bubbleSort(adil);
+
     }
 }
