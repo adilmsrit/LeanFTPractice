d59828e1eb24a32c1b2c49b78ccda6bc55881ad6
Mohammed Adil Nazeer <mohammed.nazeer@emaratech.ae>
2019-11-18 16:28:52 +0400

    Dynamic Binding example

---
+++ Practice/src/main/java/Ploymorphism/StaticAndDynamicBinding/DynamicBinding/Boy.java
@@ -0,0 +1,18 @@
+package Ploymorphism.StaticAndDynamicBinding.DynamicBinding;
+
+public class Boy extends Human {
+    public void walk(){
+        System.out.println("Boy walks");
+    }
+
+    public static void main(String... args) {
+        Human humanObj1 = new Human();
+        Human boyObj2 = new Boy();
+
+        humanObj1.walk();
+        boyObj2.walk();
+
+    }
+
+
+}
---
+++ Practice/src/main/java/Ploymorphism/StaticAndDynamicBinding/DynamicBinding/Human.java
@@ -0,0 +1,7 @@
+package Ploymorphism.StaticAndDynamicBinding.DynamicBinding;
+
+public class Human {
+    public void walk() {
+        System.out.println("Human walks");
+    }
+}
