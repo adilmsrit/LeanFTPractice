2009c21ff2c08dac0e2463a9522696c2d0078d6f
Mohammed.Nazeer <mohammed.nazeer@emaratech.ae>
2019-11-21 22:54:33 +0400

    FindDuplicateElementsInArray using HashSet

--- Practice/src/main/java/JavaInterPrograms/FindDuplicateElementsInArray.java
+++ Practice/src/main/java/JavaInterPrograms/FindDuplicateElementsInArray.java
@@ -1,8 +1,11 @@
 package JavaInterPrograms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FindDuplicateElementsInArray {
     public static void main(String... args) {
-        String[] str = {"e", "e", "b", "c", "a"};
+        String[] str = {"C++", "Go", "Adil", "Ruby", "Adil"};
         int count = 0;
         String duplicate = "";
         for (int i = 0; i < str.length - 1; i++) {
@@ -15,5 +18,13 @@ public class FindDuplicateElementsInArray {
         }
         if (count > 0) System.out.println("Duplicates present, Duplicate is :: "+duplicate);
         else System.out.println("No Duplicates");
+
+
+        //2. Using HashSet.
+        Set<String> set = new HashSet<String>();
+        for (String s : str ) {
+            if( (set.add(s))==false) System.out.println("Using Set Duplicates present, Duplicate is :: "+s);
+
+        }
     }
 }
