b249bbe330ea453efc2494d9a1cf4bfc14b4f07a
Mohammed Adil Nazeer <mohammed.nazeer@emaratech.ae>
2019-11-25 10:05:29 +0400

    Check String is Palindrome.

--- Practice/src/main/java/JavaInterPrograms/PalindromeNumber.java
+++ Practice/src/main/java/JavaInterPrograms/PalindromeNumber.java
@@ -10,6 +10,7 @@ public class PalindromeNumber {
         } else{
             System.out.println("Number is NOT palindrome :: " + number);
         }
+        checkPalindromeString("AdilidA");
     }
 
     public static int checkPalindromeNumber(int num) {
@@ -19,6 +20,16 @@ public class PalindromeNumber {
             num = num / 10;
         }
         System.out.println("Reverse of the number is :: " + rev);
+        return rev;
+    }
+
+    public static String checkPalindromeString(String str) {
+        String rev = "";
+        for (int i = str.length()-1; i >=0 ; i--) {
+            rev = rev + str.charAt(i);
+        }
+        if (str.equals(rev)) System.out.println("String is Palindrome");
+        else System.out.println("String is NOT palindrome");
         return rev;
     }
 }
