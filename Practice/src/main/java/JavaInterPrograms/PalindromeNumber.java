7931e57bf5160b0b25df08c2fe9549de85e41933
Mohammed Adil Nazeer <mohammed.nazeer@emaratech.ae>
2019-11-25 09:53:57 +0400

    New Branch.

--- Practice/src/main/java/JavaInterPrograms/PalindromeNumber.java
+++ Practice/src/main/java/JavaInterPrograms/PalindromeNumber.java
@@ -14,7 +14,6 @@ public class PalindromeNumber {
 
     public static int checkPalindromeNumber(int num) {
         int rev = 0;
-
         while (num > 0) {
             rev = (rev * 10) + (num % 10);
             num = num / 10;
