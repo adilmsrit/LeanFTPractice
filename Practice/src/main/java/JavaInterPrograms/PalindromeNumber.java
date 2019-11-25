1eef2fc64b1f14e3d75065050b5e0c1607c7ebc4
Mohammed Adil Nazeer <mohammed.nazeer@emaratech.ae>
2019-11-25 09:49:32 +0400

    Check Number is Palindrome.

---
+++ Practice/src/main/java/JavaInterPrograms/PalindromeNumber.java
@@ -0,0 +1,25 @@
+package JavaInterPrograms;
+
+public class PalindromeNumber {
+
+    public static void main(String... args) {
+        int number = 12321;
+        int result = checkPalindromeNumber(number);
+        if(result==number){
+            System.out.println("Number is palindrome :: " + number);
+        } else{
+            System.out.println("Number is NOT palindrome :: " + number);
+        }
+    }
+
+    public static int checkPalindromeNumber(int num) {
+        int rev = 0;
+
+        while (num > 0) {
+            rev = (rev * 10) + (num % 10);
+            num = num / 10;
+        }
+        System.out.println("Reverse of the number is :: " + rev);
+        return rev;
+    }
+}
